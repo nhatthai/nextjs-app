c8b169b38e808713147d4bf3513f7c4ff6f5b4b7
Nhat <nhat.thaiquang@asnet.com.vn>
2023-04-07 13:38:13 +0700

    use standalone

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} **/
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 }
 
-module.exports = nextConfig
+//module.exports = nextConfig
+// deploying a Next.js Hybrid application with more than 100Mb app size, use the standalone
+module.exports = {
+  output:"standalone",
+}
